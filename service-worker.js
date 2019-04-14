3ee15c05fdf6608359aa5931a5eec9ea48c04b06
Bastiaan <bastiaan.boland@gmail.com>
2019-04-14 12:31:10 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+// https://developers.google.com/web/fundamentals/codelabs/your-first-pwapp/
+
+var dataCacheName = 'fitdexData-v1';
+var cacheName = 'fitdexCache';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/main.js',
+  '/styles.css',
+  '/settings.svg',
+  '/cardSprite.svg'
+];
